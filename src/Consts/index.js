1e2064a071f9f68e907d1a88dade9f6ce6dd7f94
TalhaYamin <talhayamin.satti@yahoo.com>
2023-06-23 15:07:37 -0400

    deployment changes

--- src/Consts/index.js
+++ src/Consts/index.js
@@ -1,35 +1,124 @@
 export const sectorsItems2 = [
 
     {
-        id: 5,
-        name: "Manufacturing",
-        value: "1",
-        categories: [
-            { id: 51, name: "Construction materials", value: "19" },
-            { id: 52, name: "Electronics and Optics", value: "18" },
+        "sectorName": "Manufacturing",
+        "value": "11",
+        "categories": [
             {
-                id: 53,
-                name: "Food and Beverage",
-                value: "6",
-                categories: [
-                    { id: 61, name: "Bakery & confectionery products ", value: "342" },
-                    { id: 62, name: "Fish & fish products ", value: "43" },
-                    { id: 63, name: "Meat & meat products", value: "42" },
-                    { id: 64, name: "Milk & dairy products", value: "40" },
-                    { id: 65, name: "Others", value: "39", categories: [{ id: '391', name: 'abc', value: 'abc' }] },
-                    { id: 66, name: "Sweets and snack food", value: "437" },
-                ],
+                "sectorName": "Construction materials",
+                "value": "19"
             },
             {
-                id: 54, name: "Furniture", value: "13",
-                categories: [
-                    { id: 131, name: "Bathroom/sauna ", value: "389" },
-                    { id: 132, name: "Bedroom", value: "437" },
-                    { id: 133, name: "Sweets and snack food", value: "4371" },
+                "sectorName": "Electronics and Optics",
+                "value": "18"
+            },
+            {
+                "sectorName": "Food and Beverage",
+                "value": "6",
+                "categories": [
+                    {
+                        "sectorName": "Bakery & confectionery products",
+                        "value": "342"
+                    },
+                    {
+                        "sectorName": "Beverages",
+                        "value": "43"
+                    },
+                    {
+                        "sectorName": "Fish fish products",
+                        "value": "42"
+                    }, {
+                        "sectorName": "Meat meat products",
+                        "value": "40"
+                    }, {
+                        "sectorName": "Milk dairy products",
+                        "value": "39"
+                    }, {
+                        "sectorName": "Other",
+                        "value": "437"
+                    }, {
+                        "sectorName": "Sweets snack food",
+                        "value": "378"
+                    }
                 ]
             },
+            {
+                "sectorName": "Furniture",
+                "value": "13",
+                "categories": [
+                    {
+                        "sectorName": "Bathroom/sauna",
+                        "value": "389"
+                    },
+                    {
+                        "sectorName": "Bedroom",
+                        "value": "385"
+                    },
+                    {
+                        "sectorName": "Children’s room",
+                        "value": "390"
+                    }, {
+                        "sectorName": "Kitchen",
+                        "value": "98"
+                    }, {
+                        "sectorName": "Living room",
+                        "value": "101"
+                    }, {
+                        "sectorName": "Office",
+                        "value": "392"
+                    }, {
+                        "sectorName": "Outdoor",
+                        "value": "341"
+                    }
+                ]
+            },
+            {
+                "sectorName": "Machinery",
+                "value": "12",
+                "categories": [
+                    {
+                        "sectorName": "Machinery components",
+                        "value": "94"
+                    },
+                    {
+                        "sectorName": "Machinery equipment/tools",
+                        "value": "91"
+                    },
+                    {
+                        "sectorName": "Manufacture of machinery",
+                        "value": "224"
+                    }, {
+                        "sectorName": "Maritime",
+                        "value": "97",
+                        "categories": [
+                            {
+                                "sectorName": "Aluminium and steel workboats",
+                                "value": "271"
+                            },
+                            {
+                                "sectorName": "Boat/Yacht building",
+                                "value": "269"
+                            }, {
+                                "sectorName": "Ship repair and conversion",
+                                "value": "230"
+                            }
+                        ]
+                    },
+                    {
+                        "sectorName": "Metal structures",
+                        "value": "93"
+                    }, {
+                        "sectorName": "Other",
+                        "value": "508"
+                    }, {
+                        "sectorName": "Repair and maintenance service",
+                        "value": "227"
+                    }
+                ]
+            }
 
-        ],
-    },
+
+        ]
+    }
 ];
 
